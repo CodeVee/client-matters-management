8bc03223dbc8d4a2248f36c868886b51f55e8300
Victor Onyebuchi <69518362+vonyebuchi@users.noreply.github.com>
2020-10-04 18:19:58 +0100

    Configured depencies in the IOC container

---
+++ Persistence/DependencyInjection.cs
@@ -0,0 +1,27 @@
+﻿using Application.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Persistence.Context;
+using Persistence.Services;
+
+namespace Persistence
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddPersistence(this IServiceCollection services, IConfiguration configuration)
+        {
+            //services.AddDbContext<ApplicationContext>(options =>
+            //    options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"), 
+            //            b => b.MigrationsAssembly(typeof(ApplicationContext).Assembly.FullName)));
+
+            services.AddDbContext<ApplicationContext>(options =>
+                options.UseSqlite("Data Source=ClientMatters.db",
+                        b => b.MigrationsAssembly(typeof(ApplicationContext).Assembly.FullName)));
+
+            services.AddScoped<IRepositoryWrapper, RepositoryWrapper>();
+
+            return services;
+        }
+    }
+}
--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -2,23 +2,125 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Application.Interfaces;
+using AutoMapper;
+using FluentValidation.AspNetCore;
+using MicroElements.Swashbuckle.FluentValidation;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+using Newtonsoft.Json.Serialization;
+using Persistence;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace WebAPI
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
+        public Startup(IConfiguration configuration)
         {
+            Configuration = configuration;
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddPersistence(Configuration);
+            services.AddHttpContextAccessor();
+
+            #region Controller
+            services.AddControllers(setupAction =>
+            {
+                setupAction.ReturnHttpNotAcceptable = true;
+
+            }).AddNewtonsoftJson(setupAction =>
+            {
+                setupAction.SerializerSettings.ContractResolver =
+                   new CamelCasePropertyNamesContractResolver();
+            })
+             .AddXmlDataContractSerializerFormatters()
+            .AddFluentValidation(s =>
+            {
+                s.RegisterValidatorsFromAssemblyContaining<IClientRepository>();
+                s.RunDefaultMvcValidationAfterFluentValidationExecutes = false;
+                s.ValidatorFactoryType = typeof(HttpContextServiceProviderValidatorFactory);
+            })
+            .ConfigureApiBehaviorOptions(setupAction =>
+            {
+                setupAction.InvalidModelStateResponseFactory = context =>
+                {
+                    var errorsInModelState = context.ModelState
+                    .Where(x => x.Value.Errors.Count > 0)
+                    .ToDictionary(kvp => kvp.Key, kvp => kvp.Value.Errors.Select(x => x.ErrorMessage)).ToArray();
+
+                    var errorResponse = new ErrorDetails()
+                    {
+                        StatusCode = StatusCodes.Status400BadRequest,
+                        Message = "One or more validation errors occurred."
+                    };
+
+                    foreach (var error in errorsInModelState)
+                    {
+                        foreach (var subError in error.Value)
+                        {
+                            var errorModel = new ErrorModel
+                            {
+                                FieldName = error.Key,
+                                Message = subError
+                            };
+
+                            errorResponse.Errors.Add(errorModel);
+                        }
+                    }
+
+                    return new BadRequestObjectResult(errorResponse)
+                    {
+                        ContentTypes = { "application/json" }
+                    };
+                };
+            });
+            #endregion
+
+            #region Swagger
+            services.AddSwaggerGen(c =>
+            {
+                c.IncludeXmlComments(string.Format(@"{0}\ClientMatters.xml", AppDomain.CurrentDomain.BaseDirectory));
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "ClientMatters API",
+                    Description = "An API For Managing Client and their Associated Matters",
+                    Contact = new OpenApiContact()
+                    {
+                        Email = "victorchike247@gmail.com",
+                        Name = "Victor Onyebuchi",
+                        Url = new Uri("https://www.github.com/CodeVee")
+                    },
+                });
+                c.AddFluentValidationRules();
+                c.SchemaFilter<ModelSchemaFilter>();
+
+            });
+            #endregion
+
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy",
+                builder => builder.AllowAnyOrigin()
+                            .AllowAnyMethod()
+                            .AllowAnyHeader());
+            });
+
+            services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+        }
+
+
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -26,14 +128,24 @@ namespace WebAPI
                 app.UseDeveloperExceptionPage();
             }
 
+            app.ConfigureExceptionHandler();
+            app.UseCors("CorsPolicy");
+            app.UseHttpsRedirection();
+
             app.UseRouting();
+
+            #region Swagger
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "ClientMatters API");
+            });
+            #endregion
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllers();
             });
         }
     }
