6112ca955f61f3724975faff0534ea3a50d06e8a
Victor Onyebuchi <69518362+vonyebuchi@users.noreply.github.com>
2020-10-04 16:40:16 +0100

    Added Domain Entities

---
+++ Domain/Entities/Client.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Common;
+using System;
+using System.Collections.Generic;
+
+namespace Domain.Entities
+{
+    public class Client : BaseEntity
+    {
+        public Client()
+        {
+            Matters = new HashSet<Matter>();
+        }
+        public string ClientName { get; set; }
+        public string ClientCode { get; set; }
+        public DateTime RegistrationDate { get; set; }
+        public ICollection<Matter> Matters { get; private set; }
+    }
+}
---
+++ Domain/Entities/Matter.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Common;
+
+namespace Domain.Entities
+{
+    public class Matter : BaseEntity
+    {
+        public string MatterTitle { get; set; }
+        public int Amount { get; set; }
+        public string MatterCode { get; set; }
+        public string ClientCode { get; set; }
+        public Client Client { get; set; }
+    }
+}
