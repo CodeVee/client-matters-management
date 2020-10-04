b088da46d3b718853e1334ae8261cfaeeb721ab0
Victor Onyebuchi <69518362+vonyebuchi@users.noreply.github.com>
2020-10-04 17:51:07 +0100

    Added Configuration for Entities

---
+++ Persistence/Configurations/ClientConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Persistence.Configurations
+{
+    public class ClientConfiguration : IEntityTypeConfiguration<Client>
+    {
+        public void Configure(EntityTypeBuilder<Client> builder)
+        {
+            builder.Property(x => x.ClientName)
+                .IsRequired()
+                .HasMaxLength(30);
+
+            builder.Property(x => x.ClientCode)
+                .HasMaxLength(5).IsFixedLength();
+
+            builder.HasAlternateKey(x => x.ClientCode)
+                .HasName("AlternateKey_ClientCode");
+        }
+    }
+}
---
+++ Persistence/Configurations/MatterConfiguration.cs
@@ -0,0 +1,33 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Persistence.Configurations
+{
+    public class MatterConfiguration : IEntityTypeConfiguration<Matter>
+    {
+        public void Configure(EntityTypeBuilder<Matter> builder)
+        {
+            builder.Property(x => x.MatterTitle)
+                .IsRequired()
+                .HasMaxLength(50);
+
+            builder.Property(x => x.MatterCode)
+                .HasMaxLength(5).IsFixedLength();
+
+            builder.Property(x => x.ClientCode)
+                .HasMaxLength(5).IsFixedLength();
+
+            builder.HasAlternateKey(x => x.MatterCode)
+                .HasName("AlternateKey_MatterCode");
+
+            builder.HasOne(x => x.Client)
+                .WithMany(x => x.Matters)
+                .HasForeignKey(x => x.ClientCode)
+                .HasPrincipalKey(x => x.ClientCode)
+                .IsRequired()
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK_Matters_Clients");
+        }
+    }
+}
